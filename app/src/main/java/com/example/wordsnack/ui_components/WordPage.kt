d6d2982c45de221e956a5d7efccafe1aedd82d25
Radomyr Klymov <r.klymov@jobware.de>
2024-11-08 14:54:45 +0100

    stylization

--- app/src/main/java/com/example/wordsnack/ui_components/WordPage.kt
+++ app/src/main/java/com/example/wordsnack/ui_components/WordPage.kt
@@ -1,15 +1,20 @@
 package com.example.wordsnack.ui_components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.wordsnack.R
 
@@ -30,29 +36,41 @@ object WordPage {
         transcription: String,
         translation: String
     ){
-        Column {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
+        Box {
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(50.dp)
+                    .height(150.dp)
                     .background(Color.Yellow)
                     .padding(10.dp)
             ) {
-                Text(text = word)
+                Text(text = word,
+                    fontSize = 28.sp,
+                    modifier = Modifier
+                        .background(color = Color.Red)
+                        .padding(10.dp)
+                    )
                 Text(text = partOfSpeech)
                 Text(text = transcription)
                 Text(text = translation)
             }
 
             Button(
-                onClick = { navController.popBackStack() }
+                onClick = { navController.popBackStack() },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Cyan),
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(60.dp, 30.dp)
+
             ) {
-                Text(text = "BACK")
                 Icon(
                     Icons.Rounded.ArrowBack,
-                    contentDescription = stringResource(id = R.string.app_name)
+                    contentDescription = stringResource(id = R.string.app_name),
+                    modifier = Modifier
+                        .size(70.dp)
+
                 )
 
             }
